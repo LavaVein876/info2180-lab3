7ae2847efa8e38664e4f7abc343051c4f21bdaea
LavaVein <ajbirch569@gmail.com>
2025-10-17 12:07:30 -0500

    Added requirements for exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,8 +6,28 @@ window.addEventListener('DOMContentLoaded', () => {
     cells.forEach(cell => cell.classList.add('square'));
 
 
+    const status = document.getElementById('status');
     const gameState = Array(9);
     let next = true
+
+    const winCheck = (i, j, k) => {
+        const v = gameState[i];
+        return (v && v === gameState[j] && v === gameState[k]) ? v : null;
+        
+    };
+
+    const diagonalWinner = () => {
+        return winCheck(0,4,8) || winCheck(2,4,6);
+    };
+
+    const columnWinner = () => {
+        return winCheck(0,3,6) || winCheck(1,4,7) || winCheck(2,5,8);
+    };
+
+    const rowWinner = () => {
+        return winCheck(0,1,2) || winCheck(3,4,5) || winCheck(6,7,8);
+    };
+
     cells.forEach((cell, i) => {
         cell.addEventListener('click', () => {
             if (gameState[i]) return;
@@ -16,6 +36,12 @@ window.addEventListener('DOMContentLoaded', () => {
             cell.textContent = player;
             cell.classList.add(player);
             next = !next;
+
+            const winner = diagonalWinner() || rowWinner() || columnWinner();
+            if (winner){
+                status.textContent = `Congratulations! ${winner} is the winner!`;
+                status.classList.add('you-won');
+            }
         });
     });
 
@@ -28,12 +54,6 @@ window.addEventListener('DOMContentLoaded', () => {
             cell.classList.remove('hover')
         });
     });
-
-
-
-
-
-
 
 
 });
