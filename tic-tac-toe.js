2893356b7ad10c75c13434f2fee363e89b19b027
LavaVein <ajbirch569@gmail.com>
2025-10-16 23:33:38 -0500

    Added requirements for exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,4 +4,26 @@ window.addEventListener('DOMContentLoaded', () => {
     const board = document.getElementById('board');
     const cells = board.querySelectorAll('div');
     cells.forEach(cell => cell.classList.add('square'));
+
+
+    const gameState = Array(9);
+    let next = true
+    cells.forEach((cell, i) => {
+        cell.addEventListener('click', () => {
+            if (gameState[i]) return;
+            const player = next? 'X' : 'O';
+            gameState[i] = player;
+            cell.textContent = player;
+            cell.classList.add(player);
+            next = !next;
+        });
+    });
+
+
+
+
+
+
+
+
 });
